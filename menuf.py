e11c5bf3595f936e5440c559dacb9a6f3782e4f2
LIPEZIN <86574495+LipezinAndSweet@users.noreply.github.com>
2021-08-19 15:20:59 -0300

    Update menuf.py

--- menuf.py
+++ menuf.py
@@ -1,7 +1,87 @@
-import marshal, os
-try:
-	import lib, base64
-except:
-	os.system('pip install lib');os.system('pip install base64')
-xvideo = (base64.b64decode('''IyMjIyMjIyMjIyMjIyMjIyMjIyMjCiMgRlVOw4fDlUVTICYgTU9EVUxPUyAjCiMjIyMjIyMjIyMjIyMjIyMjIyMjIwoKCmdsb2JhbCBSLEIsQyxZLEcsUlQsQ1ksQ08KQ089J1wwMzNbbSc7Uj0nXDAzM1sxOzMxbSc7Qj0nXDAzM1sxOzM0bSc7Qz0nXDAzM1sxOzM3bSc7Q1k9J1wwMzNbMTszNm0nO1k9J1wwMzNbMTszM20nO0c9J1wwMzNbMTszMm0nO1JUPSdcMDMzWzswbSc7Tk9fRk9STUFUPSJcMDMzWzBtIjtDX0dSRVk4OT0iXDAzM1szODs1OzI1NG0iO0NfUkVEMT0iXDAzM1s0ODs1OzE5Nm0iCmltcG9ydCBvcywgc3lzLCBweWZpZ2xldApmcm9tIHRpbWUgaW1wb3J0IHNsZWVwCgpkZWYgY2xlYXIoKToKCW9zLnN5c3RlbSgnY2xlYXInKQoKCiMjIyMjIyMjIyMjIyMjIyMjIwojIE1FTlUgUFJJTkNJUEFMICMKIyMjIyMjIyMjIyMjIyMjIyMjCgpwcmludChmJ3tHfSAgICAgIC0tLSBGRUlUTyBQT1Ige0J9IHtHfSBMSVBFWklOe0J9IDwvPiB7R30tLS0nKQpyZXN1bHQgPSBweWZpZ2xldC5maWdsZXRfZm9ybWF0KCJMaXBlTWVudSIsIGZvbnQgPSAiY29zbWljIiApCnByaW50KGYnJyd7Q317R30Ke3Jlc3VsdH0nJycpCnByaW50KGYnICAgIHtDfT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09JykKCnByaW50KGYnICAgIHtDfT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09JykKcHJpbnQoZid7Q30gICAgW3tHfSAxe0N9IF17Un0gQkFJWEFSIE1FVEFTUExPSVQoRE8gIEd1c2htYXp1a28pIHtHfScpCnByaW50KGYne0N9ICAgIFt7R30gMntDfSBde1J9IE5NQVAnKQpwcmludChmJ3tDfSAgICBbe0d9IDN7Q30gXXtSfVRIRUhBUlZFU1RFUicpCnByaW50KGYne0N9ICAgIFt7R30gNHtDfSBde1J9IGNyaWFkb3InKQpwcmludChmJ3tDfSAgICBbe0d9IDV7Q30gXXtSfSBNRVUgQ0hBVCcpCnByaW50KGYne0N9ICAgIFt7R30gNntDfSBde1J9IFJPT1QgTk8gVEVSTVVYJykKcHJpbnQoZicgICAge0N9PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT0nKQpwcmludChmJ3tDfSAgIFt7Un0gKiB7Q31dIERJR0lURSBBIE9QQ0FPIFBPUiBOVU1FUk8nKQpvcGMgPSBpbnB1dChmJ3tHfVxuXG4gICAtLT4gU0VMRUNJT05FIEEgT1DDh8ODTyBERVNFSkFEQTogJykKCgoKaWYgb3BjID09ICcxJzoKCW9zLnN5c3RlbSgnJycKCXBrZyBpbnN0YWxsIHBvc3RncmVzcWwKCnBrZyBpbnN0YWxsIG9wZW5zc2ggd2dldCBjdXJsIGdpdCAteQoKd2dldCBodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vZ3VzaG1henVrby9tZXRhc3Bsb2l0X2luX3Rlcm11eC9tYXN0ZXIvbWV0YXNwbG9pdC5zaAoKY2htb2QgK3ggbWV0YXNwbG9pdC5zaAoKLi9tZXRhc3Bsb2l0LnNoJycnKQoKCmlmIG9wYyA9PSAnNCc6Cglvcy5zeXN0ZW0oJ3Rlcm11eC1vcGVuLXVybCBodHRwczovL1dBLm1lLys1NTM1ODgzMTc2ODEnKQoJCglyZXN1bHQgPSBweWZpZ2xldC5maWdsZXRfZm9ybWF0KCIgTWVudSIsIGZvbnQgPSAiY29zbWljIiApCgoKCmlmIG9wYyA9PSAnMyc6Cglvcy5zeXN0ZW0oJ2dpdCBjbG9uZSBodHRwczovL2dpdGh1Yi5jb20vbGFyYW1pZXMvdGhlSGFydmVzdGVyJykKCQoJCglpZiBvcGMgPT0gJzInOgoJCW9zLnN5c3RlbSgnZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9ubWFwL25tYXAnKQoJCQpyZXN1bHQgPSBweWZpZ2xldC5maWdsZXRfZm9ybWF0KCIgQlkgTElQRSIsIGZvbnQgPSAiY29zbWljIiApCnByaW50KGYnJyd7Q317R30Ke3Jlc3VsdH0nJycpCgppZiBvcGMgPT0gJzUnOgoJCW9zLnN5c3RlbSgndGVybXV4LW9wZW4tdXJsIGh0dHBzOi8vY2hhdC53aGF0c2FwcC5jb20vRkNKTTdib3pQT01BSnd3V3plQVU1aycpCgkJCgkJCmlmIG9wYyA9PSAnNic6Cglvcy5zeXN0ZW0oJycnCglhcHQgdXBkYXRlICYmIGFwdCAteSB1cGdyYWRlCgpwa2cgaW5zdGFsbCAteSBnaXQKCnBrZyBpbnN0YWxsIC15IHByb290Cgp0ZXJtdXgtc2V0dXAtc3RvcmFnZQoKZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9Bbm9ueW1vdXMtWnB0L1Qtcm9vdAoKY2QgVC1yb290CgpiYXNoIGluc3RhbGwuc2gKCi4vc3RhcnQgJycnKQ=='''))
-exec(xvideo)
+#####################
+# FUNÇÕES & MODULOS #
+#####################
+
+
+global R,B,C,Y,G,RT,CY,CO
+CO='\033[m';R='\033[1;31m';B='\033[1;34m';C='\033[1;37m';CY='\033[1;36m';Y='\033[1;33m';G='\033[1;32m';RT='\033[;0m';NO_FORMAT="\033[0m";C_GREY89="\033[38;5;254m";C_RED1="\033[48;5;196m"
+import os, sys, pyfiglet
+from time import sleep
+
+def clear():
+	os.system('clear')
+
+
+##################
+# MENU PRINCIPAL #
+##################
+
+print(f'{G}      --- FEITO POR {B} {G} LIPEZIN{B} </> {G}---')
+result = pyfiglet.figlet_format("Home", font = "cosmic" )
+print(f'''{C}{G}
+{result}''')
+print(f'    {C}======================================')
+print(f'{C}    [{G} 1{C} ]{R} BAIXAR METASPLOIT( Gushmazuko) {G}')
+print(f'{C}    [{G} 2{C} ]{R} NMAP')
+print(f'{C}    [{G} 3{C} ]{R}THEHARVESTER')
+print(f'{C}    [{G} 4{C} ]{R} CRIADOR')
+print(f'{C}    [{G} 5{C} ]{R} MEU CHAT')
+print(f'{C}    [{G} 6{C} ]{R} ROOT NO TERMUX')
+print(f'    {C}======================================')
+print(f'{C}   [{R} * {C}] DIGITE A OPCAO POR NUMERO')
+opc = input(f'{G}\n\n   --> SELECIONE A OPÇÃO DESEJADA: ')
+
+
+
+if opc == '1':
+	os.system('''
+	pkg install postgresql
+
+pkg install openssh wget curl git -y
+
+wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/metasploit.sh
+
+chmod +x metasploit.sh
+
+./metasploit.sh''')
+
+
+if opc == '4':
+	os.system('termux-open-url https://WA.me/+553588317681')
+	
+	result = pyfiglet.figlet_format(" Menu", font = "cosmic" )
+
+
+
+if opc == '3':
+	os.system('git clone https://github.com/laramies/theHarvester')
+	
+	
+	if opc == '2':
+		os.system('git clone https://github.com/nmap/nmap')
+		
+result = pyfiglet.figlet_format(" BY LIPE", font = "cosmic" )
+print(f'''{C}{G}
+{result}''')
+
+if opc == '5':
+		os.system('termux-open-url https://chat.whatsapp.com/FCJM7bozPOMAJwwWzeAU5k')
+		
+		
+if opc == '6':
+	os.system('''
+	apt update && apt -y upgrade
+
+pkg install -y git
+
+pkg install -y proot
+
+termux-setup-storage
+
+git clone https://github.com/Anonymous-Zpt/T-root
+
+cd T-root
+
+bash install.sh
+
+./start ''')
